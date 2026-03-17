fe6842ea7df673b78831ce2487c3f9ed5fe65b94
Banele Sobantu <BaneleS@betsoftware.com>
2026-03-17 10:58:24 +0200

    8.1
    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Exercise 8.1
+public class Program
+{
+    static void Main()
+    {
+        // TODO:
+        // 1. Print a Windows path using a verbatim string
+        Console.WriteLine(@"C:\Users\BaneleS\Downloads\C# Bootcamp\Lesson3");
+        // 2. Print a sentence that includes double quotes
+        Console.WriteLine(@"Good ""Morning""");
+        // 3. Print a 2-line message using a newline escape sequence
+        Console.WriteLine("Today is a Tudesday\nHappy Tuesday!!");
+    }
+}
