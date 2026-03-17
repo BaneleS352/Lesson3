80c32cfb760ca8802c82db6ac5248ab8170bb455
Banele Sobantu <BaneleS@betsoftware.com>
2026-03-17 11:10:24 +0200

    8.2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+//Exercise 8.1
+public class Program
+{
+    static void Main()
+    {
+        // TODO:
+        // 1. Print a Windows path using a verbatim string
+        Console.WriteLine(@"C:\Users\BaneleS\Downloads\C# Bootcamp\Lesson3");
+        // 2. Print a sentence that includes double quotes
+        Console.WriteLine(@"Good ""Morning""");
+        // 3. Print a 2-line message using a newline escape sequence
+        Console.WriteLine("Today is a Tudesday\nHappy Tuesday!!");
+
+        //Exercise 8.2
+        // TODO:
+        // 1. Ask for a command and normalize it
+        Console.Write("Type a command (Add / Remove / Quit): ");
+        string command = (Console.ReadLine() ?? "").Trim().ToUpperInvariant();
+
+        if (command == "ADD")
+        {
+            Console.WriteLine("You chose ADD.");
+        }
+        else if (command == "REMOVE")
+        {
+            Console.WriteLine("You chose REMOVE.");
+        }
+        else if (command == "QUIT")
+        {
+            Console.WriteLine("Goodbye!");
+        }
+        else
+        {
+            Console.WriteLine("Unknown command.");
+        }
+    }
+}
